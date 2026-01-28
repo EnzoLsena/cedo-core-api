e48d0a45730139fdd70129825bf6cedc3b687a5c
EnzoLsena <enzolevysena@gmail.com>
2026-01-28 00:40:57 -0300

    chore: add initial database migration

--- src/database/typeorm.config.ts
+++ src/database/typeorm.config.ts
@@ -1,18 +1,18 @@
+import 'dotenv/config';
 import { DataSource } from 'typeorm';
-import * as dotenv from 'dotenv';
-dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 
-  entities: [__dirname + '/../**/*.entity.{ts,js}'],
-  migrations: [__dirname + '/migrations/*.{ts,js}'],
+  entities: ['src/**/*.entity.ts'],
+  migrations: ['src/database/migrations/*.ts'],
 
   synchronize: false,
   logging: true,
 });
+
