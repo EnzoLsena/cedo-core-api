ba510211156fd0debef5adeab8afdf38169c31bf
EnzoLsena <enzolevysena@gmail.com>
2026-01-28 01:44:13 -0300

    fix: update database configuration to use correct environment variable names

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,7 +3,7 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('database', () => ({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  pass: process.env.DB_PASS,
+  user: process.env.DB_USERNAME,
+  pass: process.env.DB_PASSWORD,
   name: process.env.DB_NAME,
 }));
