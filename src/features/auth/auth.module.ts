5681913a5d45d4b06f49a145b0a2fdbb4ee3ea49
EnzoLsena <enzolevysena@gmail.com>
2026-01-29 18:58:31 -0300

    feat: integrate JWT configuration and update AuthService to use it

--- src/features/auth/auth.module.ts
+++ src/features/auth/auth.module.ts
@@ -4,10 +4,12 @@ import { AuthController } from './auth.controller';
 import { HashingServiceProtocol } from './hashing/hashing.service';
 import { BCryptService } from './hashing/bcrypt.service';
 import { UserModule } from '../user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from './config/jwt.config';
 
 @Global()
   @Module({
-  imports:[UserModule],
+  imports:[UserModule, ConfigModule.forFeature(jwtConfig)],
   controllers: [AuthController],
   providers: [
     AuthService,
--- src/features/auth/auth.service.ts
+++ src/features/auth/auth.service.ts
@@ -1,13 +1,17 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { LoginCredentialsDto } from './dto/login-credentials.dto';
 import { UserService } from '../user/user.service';
 import { HashingServiceProtocol } from './hashing/hashing.service';
+import jwtConfig from './config/jwt.config';
+import { ConfigType } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
   constructor(
+    @Inject(jwtConfig.KEY)
     private readonly userService: UserService,
     private readonly hashingService: HashingServiceProtocol,
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
   ) {}
 
   async login(loginCredentialsDto: LoginCredentialsDto) {
