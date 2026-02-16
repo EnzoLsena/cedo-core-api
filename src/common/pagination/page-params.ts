faca6ddfbb05f3144290ba1c50189164907b8738
EnzoLsena <enzolevysena@gmail.com>
2026-02-16 17:52:14 -0300

    feat: add pagination classes for handling page parameters and page lists

---
+++ src/common/pagination/page-params.ts
@@ -0,0 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsInt, Min } from 'class-validator';
+
+export class PageParams {
+  @ApiProperty({ default: 1 })
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  pageNumber: number = 1;
+
+  @ApiProperty({ default: 15 })
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  pageSize: number = 15;
+
+  constructor(partial?: Partial<PageParams>) {
+    Object.assign(this, partial);
+  }
+}
