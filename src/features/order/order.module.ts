5745f1d62f390cd3408a048fccb6238e05b4eeed
EnzoLsena <enzolevysena@gmail.com>
2026-02-17 12:50:09 -0300

    feat: update module imports to include necessary entities for TypeORM

--- src/features/order/order.module.ts
+++ src/features/order/order.module.ts
@@ -4,9 +4,11 @@ import { OrderController } from './order.controller';
 import { Order } from './entities/order.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderItem } from './entities/order-item.entity';
+import { Customer } from '../customer/entities/customer.entity';
+import { Product } from '../product/entities/product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, OrderItem])],
+  imports: [TypeOrmModule.forFeature([Order, OrderItem, Customer, Product])],
   controllers: [OrderController],
   providers: [OrderService],
 })
--- src/features/recipe/recipe.module.ts
+++ src/features/recipe/recipe.module.ts
@@ -4,9 +4,10 @@ import { RecipeController } from './recipe.controller';
 import { RecipeItem } from './entities/recipe-items.entity';
 import { Recipe } from './entities/recipe.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from '../product/entities/product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RecipeItem, Recipe])],
+  imports: [TypeOrmModule.forFeature([RecipeItem, Recipe, Product])],
   controllers: [RecipeController],
   providers: [RecipeService],
 })
